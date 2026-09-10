33ed7622db84af1dff17db8236ff10252b6125f4
Arturo <29128253+aig77@users.noreply.github.com>
2026-09-09 23:16:45 -0400

    feat(terminal): add terminal.lua derivation to nix shell

--- shell.nix
+++ shell.nix
@@ -1,8 +1,15 @@
-{pkgs ? import <nixpkgs> {}}:
-pkgs.mkShell {
-  nativeBuildInputs = with pkgs.buildPackages; [
-    lua
-    luarocks
-    luau-lsp
-  ];
-}
+{pkgs ? import <nixpkgs> {}}: let
+  terminalSrc = pkgs.fetchFromGitHub {
+    owner = "lunarmodules";
+    repo = "terminal.lua";
+    rev = "0.1.0";
+    hash = "sha256-9bPcaun7d5R+DgJWFySv889puFaeJ18wIBUnQ8IBu+8=";
+  };
+  luaEnv = pkgs.lua5_4.withPackages (ps: [ps.luasystem ps.utf8]);
+in
+  pkgs.mkShell {
+    nativeBuildInputs = [luaEnv pkgs.luau-lsp];
+    shellHook = ''
+      export LUA_PATH="${terminalSrc}/src/?.lua;${terminalSrc}/src/?/init.lua;;"
+    '';
+  }
